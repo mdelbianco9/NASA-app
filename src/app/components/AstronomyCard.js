a9f3df28b70e950a110d7ab1995a71f86794f94e
marcus delbianco <mdelbianco9@gmail.com>
2018-04-16 17:19:37 -0700

    added if statment for media type

--- src/app/components/AstronomyCard.js
+++ src/app/components/AstronomyCard.js
@@ -4,20 +4,50 @@ import React from 'react';
 
 const AstronomyCard = (props) => {
 
-	const { title, url, hdurl, explanation, date, copyright } = props.data;
-	return (
+	const { title, url, hdurl, explanation, date, copyright, media_type } = props.data;
 
-		<div className="astronomy-card">
+	let nasa = null;
+
+	let media = {media_type};
+	let video = "video";
+	let image = "image";
+
+	if (media = video) {
+		nasa = (
+			<div className="astronomy-card">
 			<h6 className="astronomy-title">{title}</h6>
 
 			<a href={hdurl} className="astronomy-image-wrapper">
-				<iframe height="300" width="400" src={url} alt={title} />
+				<iframe height="300" width="450" src={url} alt={title} />
 			</a>
 
 			<p>{explanation}</p>
 
 			<span>{date}, {copyright}</span>
 		</div>
+		); 
+
+		console.log("video working "+media);
+	} else if (media = image) {
+		nasa = (
+			<div className="astronomy-card">
+			<h6 className="astronomy-title">{title}</h6>
+
+			<a href={hdurl} className="astronomy-image-wrapper">
+				<img src={url} alt={title} />
+			</a>
+
+			<p>{explanation}</p>
+
+			<span>{date}, {copyright}</span>
+		</div>
+			);
+		console.log("image is working "+media);
+	} else {null}
+
+	return (
+
+		nasa
 		)
 }
 
