2d52f79ab130c692c6ff28bad71b6b92b781bacc
marcus delbianco <mdelbianco9@gmail.com>
2018-04-18 21:20:14 -0700

    Adding Redux to project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import './assets/stylesheets/styles.scss';
 
 import React from 'react'; // import the main react dependency
 import ReactDOM from 'react-dom'; // import reactDOM
+import { Provider } from 'react-redux'; //Glue for react and redux
+import Store from './Store'; //redux state store, actas as prop for Provider tag
 import App from './app/App.jsx'; // import the main app component
+import './assets/stylesheets/styles.scss'; 
 
-ReactDOM.render(<App />, document.getElementById('root')); // render our App component and mount it to our #root element
+ReactDOM.render(
+<Provider store={Store}> { /*Glue for react and redux */ }
+	<App /> { /* Render our App component */}
+</Provider>,
+document.getElementById('root')
+); 
